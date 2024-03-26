2b28475c1316e1633576609905a4414435aff71a
UzairMohammed7 <uzzumohd.777@gmail.com>
2024-03-26 18:09:34 +0000

    Simple Todo Sol

--- src/components/SimpleTodos/index.js
+++ src/components/SimpleTodos/index.js
@@ -1,3 +1,5 @@
+// eslint-disable-next-line
+import {v4 as uuidv4} from 'uuid'
 import {Component} from 'react'
 
 import TodoItem from '../TodoItem'
@@ -8,60 +10,123 @@ const initialTodosList = [
   {
     id: 1,
     title: 'Book the ticket for today evening',
+    completed: false,
   },
   {
     id: 2,
     title: 'Rent the movie for tomorrow movie night',
+    completed: false,
   },
   {
     id: 3,
     title: 'Confirm the slot for the yoga session tomorrow morning',
+    completed: false,
   },
   {
     id: 4,
     title: 'Drop the parcel at Bloomingdale',
+    completed: false,
   },
   {
     id: 5,
     title: 'Order fruits on Big Basket',
+    completed: false,
   },
   {
     id: 6,
     title: 'Fix the production issue',
+    completed: false,
   },
   {
     id: 7,
     title: 'Confirm my slot for Saturday Night',
+    completed: false,
   },
   {
     id: 8,
     title: 'Get essentials for Sunday car wash',
+    completed: false,
   },
 ]
 
 class SimpleTodos extends Component {
-  state = {todoList: initialTodosList}
+  state = {todoList: initialTodosList, newTodoTitle: '', newTodoCount: 1}
+
+  onChangeInput = event => {
+    this.setState({[event.target.name]: event.target.value})
+  }
+
+  addTodoToList = () => {
+    const {newTodoTitle, newTodoCount} = this.state
+    const newTodos = Array.from({length: newTodoCount}, (_, i) => ({
+      id: Date.now() + i,
+      title: newTodoTitle,
+      completed: false,
+    }))
+    this.setState(prevState => ({
+      todoList: [...prevState.todoList, ...newTodos],
+      newTodoTitle: '',
+      newTodoCount: 1,
+    }))
+  }
+  // const newTodos = {
+  //   id: uuidv4(),
+  //   title: newTodoTitle,
+  //   completed: false,
+  // }
 
   deleteTodoItem = id => {
     const {todoList} = this.state
     const updateTodoList = todoList.filter(eachTodo => eachTodo.id !== id)
-
     this.setState({todoList: updateTodoList})
-    console.log('triggered')
+  }
+
+  toggleComplete = id => {
+    const {todoList} = this.state
+    const updatedTodoList = todoList.map(todo =>
+      todo.id === id ? {...todo, completed: !todo.completed} : todo,
+    )
+    this.setState({todoList: updatedTodoList})
   }
 
   render() {
-    const {todoList} = this.state
+    const {todoList, newTodoTitle, newTodoCount} = this.state
     return (
       <div className="container">
         <div className="todoCardContainer">
           <h1 className="heading">Simple Todos</h1>
+          <div className="input-and-add-btn">
+            <input
+              type="text"
+              name="newTodoTitle"
+              className="input-text"
+              placeholder="Add ToDo List"
+              onChange={this.onChangeInput}
+              value={newTodoTitle}
+            />
+            <input
+              type="number"
+              name="newTodoCount"
+              className="input-text"
+              value={newTodoCount}
+              onChange={this.onChangeInput}
+              placeholder="Enter number of todo"
+            />
+            <button
+              type="button"
+              className="add-btn"
+              onClick={this.addTodoToList}
+            >
+              Add
+            </button>
+          </div>
           <ul className="todoListItems">
             {todoList.map(eachTodo => (
               <TodoItem
                 key={eachTodo.id}
                 todoDetails={eachTodo}
                 deleteTodoItem={this.deleteTodoItem}
+                toggleComplete={this.toggleComplete}
               />
             ))}
           </ul>
--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,21 +1,76 @@
+import {Component} from 'react'
 import './index.css'
 
-const TodoItem = props => {
-  const {deleteTodoItem, todoDetails} = props
-  const {id, title} = todoDetails
+class TodoItem extends Component {
+  state = {isEdited: false, titleUpdated: ''}
 
-  const onDeleteTodo = () => {
-    deleteTodoItem(id)
+  onClickEdit = () => {
+    const {todoDetails} = this.props
+    this.setState({isEdited: true, titleUpdated: todoDetails.title})
   }
 
-  return (
-    <li className="todoList">
-      <p className="title">{title}</p>
-      <button className="btn" type="button" onClick={onDeleteTodo}>
-        Delete
-      </button>
-    </li>
-  )
+  onClickSave = () => {
+    this.setState({isEdited: false})
+  }
+
+  changeTitle = event => {
+    this.setState({titleUpdated: event.target.value})
+  }
+
+  render() {
+    const {todoDetails, deleteTodoItem, toggleComplete} = this.props
+    const {isEdited, titleUpdated} = this.state
+
+    return (
+      <li className={todoDetails.completed ? 'todoList completed' : 'todoList'}>
+        {isEdited ? (
+          <>
+            <input
+              type="text"
+              value={titleUpdated}
+              onChange={this.changeTitle}
+              className="input-text"
+            />
+            <button
+              onClick={this.onClickSave}
+              type="button"
+              className="save-btn"
+            >
+              Save
+            </button>
+          </>
+        ) : (
+          <>
+            <div className="check-title">
+              <input
+                type="checkbox"
+                checked={todoDetails.completed}
+                onChange={() => toggleComplete(todoDetails.id)}
+                className="checkbox"
+              />
+              <p className="title">{todoDetails.title}</p>
+            </div>
+            <div>
+              <button
+                onClick={this.onClickEdit}
+                type="button"
+                className="edit-btn"
+              >
+                Edit
+              </button>
+              <button
+                onClick={() => deleteTodoItem(todoDetails.id)}
+                type="button"
+                className="delete-btn"
+              >
+                Delete
+              </button>
+            </div>
+          </>
+        )}
+      </li>
+    )
+  }
 }
 
 export default TodoItem
